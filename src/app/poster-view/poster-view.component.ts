3f9a8d247a2f1da40b3441838ee6c4b00decd3b3
Kerry Shetline <kerry@shetline.com>
2023-11-20 16:41:56 -0500

    Improved search, step 1.

--- src/app/poster-view/poster-view.component.ts
+++ src/app/poster-view/poster-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LibraryItem, VideoLibrary, VType } from '../../../server/src/shared-types';
 import { checksum53, hashTitle } from '../video-ui-utils';
-import { encodeForUri, stripDiacriticals_lc } from '@tubular/util';
+import { clone, encodeForUri, stripDiacriticals_lc } from '@tubular/util';
 import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,10 +10,34 @@ function isMovie(item: LibraryItem): boolean {
     (item.type === VType.COLLECTION && item.data?.length > 0 && item.data[0].type === VType.MOVIE);
 }
 
+function containsMovie(item: LibraryItem): boolean {
+  if (isMovie(item))
+    return true;
+
+  for (const child of (item.data || [])) {
+    if (containsMovie(child))
+      return true;
+  }
+
+  return false;
+}
+
 function isTV(item: LibraryItem): boolean {
   return item.isTV || item.type === VType.TV_SHOW || item.type === VType.TV_SEASON ||
       item.type === VType.TV_EPISODE || item.type === VType.TV_COLLECTION ||
       (item.type === VType.COLLECTION && item.data?.length > 0 && isTV(item.data[0]));
+}
+
+function containsTV(item: LibraryItem): boolean {
+  if (isTV(item))
+    return true;
+
+  for (const child of (item.data || [])) {
+    if (containsTV(child))
+      return true;
+  }
+
+  return false;
 }
 
 @Component({
@@ -118,15 +142,19 @@ export class PosterViewComponent implements OnInit {
 
     document.querySelector('.poster-grid').scrollTop = 0;
 
+    let matchFunction: (item: LibraryItem) => boolean;
+
     switch (this.filter) {
       case 'All':
         this.items = this.library.array.filter(item => this.matchesSearch(item));
         break;
       case 'Movies':
-        this.items = this.library.array.filter(item => this.matchesSearch(item) && isMovie(item));
+        matchFunction = containsMovie;
+        this.items = this.library.array.filter(item => this.matchesSearch(item) && matchFunction(item));
         break;
       case 'TV':
-        this.items = this.library.array.filter(item => this.matchesSearch(item) && isTV(item));
+        matchFunction = containsTV;
+        this.items = this.library.array.filter(item => this.matchesSearch(item) && matchFunction(item));
         break;
       case '4K':
         this.items = this.library.array.filter(item => this.matchesSearch(item) && item.is4k);
@@ -134,6 +162,29 @@ export class PosterViewComponent implements OnInit {
       case '3D':
         this.items = this.library.array.filter(item => this.matchesSearch(item) && item.is3d);
         break;
+    }
+
+    if (matchFunction) {
+      let hasBeenCloned = false;
+
+      for (let i = 0; i < this.items.length; ++i) {
+        const item = this.items[i];
+
+        if (item.type === VType.COLLECTION) {
+          const innerCount = item.data.reduce((sum, child) => sum + (matchFunction(child) ? 1 : 0), 0);
+
+          if (innerCount === 1)
+            this.items[i] = item.data.find(c => matchFunction(c));
+          else if (innerCount < item.data.length) {
+            if (!hasBeenCloned) {
+              this.items = clone(this.items);
+              hasBeenCloned = true;
+            }
+
+            item.data = item.data.filter(c => matchFunction(c));
+          }
+        }
+      }
     }
   }
 
