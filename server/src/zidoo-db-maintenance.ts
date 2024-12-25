03dd8fb544eebe17e90626513165ed830b8bc447
kshetline <kerry@shetline.com>
2024-12-25 01:50:49 -0500

    Add DB mediainfo updating.

--- server/src/zidoo-db-maintenance.ts
+++ server/src/zidoo-db-maintenance.ts
@@ -1,6 +1,9 @@
 import * as paths from 'path';
 import { existsAsync } from './vs-util';
 import { AsyncDatabase } from 'promised-sqlite3';
+import { toBoolean } from '@tubular/util';
+import { monitorProcess } from './process-util';
+import { spawn } from 'child_process';
 
 export async function doZidooDbMaintenance(): Promise<void> {
   const dbPath = process.env.VS_ZIDOO_DB;
@@ -10,17 +13,19 @@ export async function doZidooDbMaintenance(): Promise<void> {
 
   try {
     const db = await AsyncDatabase.open(dbPath);
-    const rows: any[] = [];
+    let rows: any[] = [];
     const missing: number[] = [];
 
     await db.each('SELECT * FROM VIDEO_INFO', undefined, (row: any) => rows.push({ uri: row.URI, id: row._id }));
 
-    for await (const row of rows) {
+    for (let i = 0; i < rows.length; ++i) {
+      const row = rows[i];
       const path = paths.join(process.env.VS_VIDEO_SOURCE, row.uri);
 
       if (!await existsAsync(path)) {
         console.log('Missing:', path);
         missing.push(row.id);
+        rows[i] = null;
       }
     }
 
@@ -28,6 +33,24 @@ export async function doZidooDbMaintenance(): Promise<void> {
 
     for await (const id of missing) {
       await db.run('DELETE FROM VIDEO_INFO WHERE _id = ?', id);
+    }
+
+    if (toBoolean(process.env.VS_ZIDOO_DB_UPDATE_MI)) {
+      rows = rows.filter(row => !!row);
+
+      for (const row of rows) {
+        const path = paths.join(process.env.VS_VIDEO_SOURCE, row.uri);
+
+        try {
+          const mediaJson = JSON.stringify(JSON.parse(await monitorProcess(spawn('mediainfo', [path, '--Output=JSON']))), null, 4);
+
+          await db.run('UPDATE VIDEO_INFO SET MEDIA_JSON = ? WHERE _id = ?', mediaJson, row.id);
+          console.log('Updated mediainfo for', path);
+        }
+        catch (e) {
+          console.error('Failed to update mediainfo for %s: %s', path, e.message);
+        }
+      }
     }
 
     console.log('DB clean-up done');
